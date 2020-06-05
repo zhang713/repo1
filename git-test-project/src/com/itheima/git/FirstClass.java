5e3c1c69c2c80cdf740d2a11a3ad1d4f67f960e7
zhb713 <1273680996@qq.com>
2020-06-05 18:55:40 +0800

    添加到git

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,16 @@
+package com.itheima.git;
+
+/**
+ * @Author: ZhangHB
+ * @Date: 2020-06-05 18:41:05
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
