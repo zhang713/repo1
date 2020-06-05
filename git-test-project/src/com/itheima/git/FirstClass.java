b369baf3b667ddf065bc72d1c60a724408d4b2e1
zhb713 <1273680996@qq.com>
2020-06-05 19:27:01 +0800

    修改类，新增属性

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -5,6 +5,37 @@ package com.itheima.git;
  * @Date: 2020-06-05 18:41:05
  */
 public class FirstClass {
+
+    private String id;
+
+    private String name;
+
+    private String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public static void main(String[] args) {
         System.out.println("hello git project");
         System.out.println("hello git project");
@@ -18,4 +49,6 @@ public class FirstClass {
         int i = 0;
         i += 10;
     }
+
+
 }
